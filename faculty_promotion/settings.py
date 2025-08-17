a6784f4eb72adacfb116ac895f0b97efe036bef0
navya-n03 <navyanallabelly@gmail.com>
2025-08-17 12:11:45 +0530

    Fix static files settings for Render

--- faculty_promotion/settings.py
+++ faculty_promotion/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,9 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT=BASE_DIR /'staticfiles'
-STATICFILES_DIRS=[BASE_DIR/"static",]
+
 
 
 # Default primary key field type
